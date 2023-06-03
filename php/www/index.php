838755584cfac8298f156f918880b9ad4a965b03
VoiteckHeira <wojciechdziecic@gmail.com>
2023-06-03 21:39:12 +0200

    robota

---
+++ php/www/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Moja strona</title>
+</head>
+
+<body>
+    <h1>Informacje o bazie danych</h1>
+
+    <hr />
+    <br />
+    <h3>Imię i nazwisko autora</h3>
+    <p>Imię: Wojciech</p>
+    <p>Nazwisko: Dziedzic</p>
+
+</body>
+
+</html>
